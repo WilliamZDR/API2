6c2fc61cd7700c2a7823c24a7c5eff8101b6a345
WilliamZDR <78449640+WilliamZDR@users.noreply.github.com>
2022-10-28 01:34:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-for flask import *
+import os
+from flask import Flask
+from flask_cors import CORS 
+
 app = Flask(__name__)
+
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
 
 @app.route("/", methods=["GET"])
 def index():
@@ -9,3 +14,6 @@ def index():
 def main():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
